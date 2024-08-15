f71012d1288ced45dc8d7c050bc2950b6aa773cd
holding-1-at-a-time <rromerojr1@gmail.com>
2024-08-15 03:37:00 -0500

    fixed issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import {
 } from '@clerk/nextjs'
 import './globals.css'
 import ConvexClerkProvider from './ConvexClerkProvider';
-import { ClerkProvider } from '@clerk/nextjs';
 
 export default function RootLayout({
   children,
