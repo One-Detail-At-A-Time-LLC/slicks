9f82bae94a3b4824cfc534a289d163de7098b305
holding-1-at-a-time <rromerojr1@gmail.com>
2024-08-14 22:43:03 -0500

    first commit

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+/**
+ * Jest configuration for Next.js projects
+ */
+
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const jestConfig: Config.InitialOptions = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['text', 'lcov'],
+}
+
+export default nextJest(jestConfig)
+
